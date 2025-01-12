23e027f128e2330dcdcb002993fedd10decc375d
TrulyCalculated <matthew.c.pulver@gmail.com>
2025-01-12 00:53:29 -0500

    Completed following exercise from Copilot, adding all functionality.: Exercise: Extend the Vehicle Program Add a new subclass:
    Create a new subclass of Vehicle called Truck.
    The Truck class should have an engineType attribute and a cargoCapacity attribute.
    The Truck class should have a drive method that prints a message including the engineType and speed.
    Modify the Vehicle class:
    
    Add a fuel attribute to the Vehicle class.
    Modify the drive method in the Vehicle class to decrease the fuel by 1 unit each time the drive method is called.
    If the fuel is 0 or less, print a message indicating that the vehicle is out of fuel and cannot drive.
    Test the new functionality:
    
    Create instances of the Truck class with different engineType and cargoCapacity.
    Set the fuel attribute for each vehicle.
    Call the drive method multiple times and observe the fuel decreasing.
    Ensure that the vehicle stops driving when out of fuel.

--- Python/pyClass.py
+++ Python/pyClass.py
@@ -1,13 +1,21 @@
 # Initializes a class. Classes allow for encapsulation of data for reuse and readability.
 class Vehicle():
     # Initialization function in class. Includes parameter bodyStyle.
-    def __init__(self, bodyStyle):
+    def __init__(self, bodyStyle, fuel = 10):
         # Object variable. Defined by the class' parameters.
         self.bodyStyle = bodyStyle
+        self.fuel = fuel
     
     def drive(self, speed):
         self.mode = "driving"
         self.speed = speed
+        # Modified drive method to complete exercise.
+        if self.fuel == 0:
+            print("This vehicle cannot drive anymore as it is out of fuel!")
+        else:
+            self.fuel = self.fuel - 1 
+
+    
 # Put the superclass name in a subclass 
 class Car(Vehicle):
     def __init__(self, engineType):
@@ -18,13 +26,32 @@ class Car(Vehicle):
 
     def drive(self, speed):
         super().drive(speed)
-        print("Driving my", self.engineType, "car at", self.speed)
+        if self.fuel > 0:
+            print("Driving my", self.engineType, "car at", self.speed)
+
+# Truck subclass for exercise as suggested by Copilot.
+class Truck(Vehicle):
+    def __init__(self, engineType, cargoCapacity):
+        super().__init__("Truck")
+        self.wheels = 4
+        self.doors = 4
+        self.engineType = engineType
+        # Requested cargo capacity attribute.
+        self.cargoCapacity = cargoCapacity
+    
+    # Requested drive method. Has same functionality as other subclasses.
+    def drive(self, speed):
+        super().drive(speed)
+        if self.fuel > 0:
+            print("Driving my", self.engineType, "truck at", self.speed)
+
+
 
 # Another subclass.
 class Motorcycle(Vehicle):
     def __init__(self, engineType, hasSideCar):
         super().__init__("Motorcycle")
-        if(hasSideCar):
+        if hasSideCar:
             self.wheels = 3
         else:
             self.wheels = 2
@@ -33,12 +60,17 @@ class Motorcycle(Vehicle):
 
     def drive(self, speed):
         super().drive(speed)
-        print("Driving my", self.engineType, "car at", self.speed)
+        if self.fuel > 0:
+            print("Driving my", self.engineType, "motorcycle at", self.speed)
+
+
     
 
 car1 = Car("gas")
 car2 = Car("electric")
 mc1 = Motorcycle("gas", True)
+truck1 = Truck("diesel", 100)
+truck2 = Truck("electric", 50)
 
 #Test prints
 print(mc1.wheels)
@@ -49,3 +81,9 @@ print(car2.doors)
 car1.drive(30)
 car2.drive(40)
 mc1.drive(50)
+
+#Truck test. Runs drive 11 times then prints cargo capacity of the two truck objects.
+for x in range(0,11):
+    truck1.drive(50)
+print(truck1.cargoCapacity)
+print(truck2.cargoCapacity)
