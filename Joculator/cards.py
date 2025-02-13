537f869fdd8d6c5c3888cd51cbec2ba3ba8cccf2
TrulyCalculated <matthew.c.pulver@gmail.com>
2025-02-13 02:04:08 -0500

    Refactored cards.py classes to comply with PEP8 styling.

--- Joculator/cards.py
+++ Joculator/cards.py
@@ -1,80 +1,136 @@
-class pokerCard:
+"""
+cards.py: Contains classes for objects in Joculator related to playing cards, 
+such as numbered cards, face cards, and aces. Will implement Jokers,
+and other cards found in Balatro later.
+"""
+class PokerCard:
+    """
+    The PokerCard object represents a standard playing card in a playing card deck.
+    Has a suit, rank, and a chip value. 3 ranks are face cards and each object can
+    have its suit, rank, or chip value dynamically changed depending on game state.
+    """
     # Valid poker suits
     suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
-    # Valid poker ranks 
+    # Valid poker ranks
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'A', 'J', 'Q', 'K']
-
     def __init__(self, suit, rank):
+        """
+        Initializes the PokerCard object with a suit, rank, and chip value.
+
+        Parameters:
+            suit (str): Specifies the suit of the playing card.
+            rank (int/str): Specifies the rank of the playing card.
+
+        Raises: ValueError if the suit or rank is not a valid suit or rank.
+        """
         # Defines suits and ranks.
-        if suit not in pokerCard.suits:
+        if suit not in PokerCard.suits:
             raise ValueError(f'Invalid suit: {suit}')
-        if rank not in pokerCard.ranks:
+        if rank not in PokerCard.ranks:
             raise ValueError(f'Invalid rank: {rank}')
         self.suit = suit
         self.rank = rank
         # Checks to see if the card has been modified in game.
-        self.isModified = False
-
-        # Chip value can be intuited based on rank value. If a face card, set to 10. If ace, set to 11. 
+        self.is_modified = False
+        # Chip value can be intuited based on rank value. If a face card, then 10. If ace, then 11.
         self.chips = rank
         if str(self.chips) in ('J', 'Q', 'K'):
             self.chips = 10
         elif str(self.chips) in ('A'):
             self.chips = 11
-        
-        # Set isFace boolean to false by default. If the rank is J, Q, or K, then set isFace to true.
-        self.isFace = False
+        # If the rank is J, Q, or K, then set is_face to true.
+        self.is_face = False
         if str(self.rank) in ('J', 'Q', 'K'):
-            self.isFace = True
+            self.is_face = True
+    def set_suit(self, suit):
+        """
+        Setter method for the suit value for a PokerCard object.
+        
+        Parameters:
+            suit (str): Suit which a PokerCard object is being changed to.
 
-    # Setter method for suit. 
-    def setSuit(self, suit):
-        if suit not in pokerCard.suits:
+        Raises: ValueError if suit is invalid.
+        """
+        if suit not in PokerCard.suits:
             raise ValueError(f'Invalid suit: {suit}')
         self.suit = suit
-        self.isModified = True
+        self.is_modified = True
+    # Setter method for rank.
+    def set_rank(self, rank):
+        """
+        Setter method for the rank value for a PokerCard object.
+        Unless the card has already been modified:
+        If the rank is a jack, queen, or king, change the chips to 10.
+        If the rank is an ace, change the chips to 11.
+        
+        Parameters:
+            rank (int,str): rank which a PokerCard object is being changed to.
 
-    # Setter method for rank. 
-    def setRank(self, rank):
-        if rank not in pokerCard.ranks:
+        Raises: ValueError if rank is invalid.
+        """
+        if rank not in PokerCard.ranks:
             raise ValueError(f'Invalid rank: {rank}')
         self.rank = rank
         # If set rank is J, Q, or K, then set chips to 10 and designates as a face card.
-        if str(self.rank) in ('J', 'Q', 'K') and self.isModified == False:
+        if str(self.rank) in ('J', 'Q', 'K') and self.is_modified is False:
             self.chips = 10
-            self.isFace = True
-
+            self.is_face = True
         # If ace, set chips to 11.
-        elif str(self.chips) in ('A') and self.isModified == False:
+        elif str(self.chips) in ('A') and self.is_modified is False:
             self.chips = 11
-        self.isModified = True
-
-    # Setter method for chips.
-    def setChips(self, chips):
+        self.is_modified = True
+    def set_chips(self, chips):
+        """
+        Setter method for the chips value for a PokerCard object.
+        
+        Parameters:
+            chips (int): The chip value which a PokerCard object is being changed to.
+        """
         self.chips = chips
-        self.isModified = True
-    
-    # Setter method for face card boolean. 
-    def setFace(self, faceCard):
-        self.isFace = faceCard
-        self.isModified = True
-
-class pokerDeck:
-    # Poker deck class will instantiate with a defaultDeck which can be changed to a selected deck.
+        self.is_modified = True
+    def set_face(self, face_card):
+        """
+        Setter method for the boolean deciding if a card is considered a face card.
+        
+        Parameters:
+            face_card (bool): Is the card a face card or not a face card?
+        """
+        self.is_face = face_card
+        self.is_modified = True
+class PokerDeck:
+    """
+    The PokerDeck object describes an object containing a list of PokerCard objects.
+    Can be configured with various custom deck types, uses a standard poker deck as
+    a default setting.
+    """
     def __init__(self):
-        self.cardDeck = []
-        self.cardCount = len(self.cardDeck)
+        """
+        Instantiates the PokerDeck object. Makes an empty card_deck list and
+        tracks the length of the deck.
+        """
+        self.card_deck = []
+        self.card_count = len(self.card_deck)
+    def default_deck(self):
+        """
+        Generates the default deck of playing cards.
+        Has one card of each acceptable suit and rank.
+        Contains 52 cards in the deck.
 
-    # Generates a standard deck of poker cards.
-    def defaultDeck(self):
+        Returns:
+            deck: A list containing every generated poker card.
+        """
         deck = []
-        # Generates a pokerCard for every pokerCard.suits and pokerCard.ranks
-        for suit in pokerCard.suits:
-            for rank in pokerCard.ranks:
-                deck.append(pokerCard(suit, rank))
+        for suit in PokerCard.suits:
+            for rank in PokerCard.ranks:
+                deck.append(PokerCard(suit, rank))
         return deck
-    
-    # Sets the card deck for the poker deck class.
-    def setDeck(self, deck):
-        self.cardDeck = deck
-        self.cardCount = len(deck)
+    def set_deck(self, deck):
+        """
+        Setter method for the deck which sets the type of deck used.
+        
+        Parameters:
+            deck (list): The type of deck being changed to.
+        """
+        self.card_deck = deck
+        self.card_count = len(deck)
+      
